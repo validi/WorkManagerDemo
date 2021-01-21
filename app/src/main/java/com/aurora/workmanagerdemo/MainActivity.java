e5214627b57574b2f0bdb33862d55282be01d9fd
monem validi <a.validi6016@gmail.com>
2021-01-21 20:04:11 +0330

    first Commit

---
+++ app/src/main/java/com/aurora/workmanagerdemo/MainActivity.java
@@ -0,0 +1,61 @@
+package com.aurora.workmanagerdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.work.Constraints;
+import androidx.work.Data;
+import androidx.work.NetworkType;
+import androidx.work.OneTimeWorkRequest;
+import androidx.work.WorkInfo;
+import androidx.work.WorkManager;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.nio.channels.NetworkChannel;
+
+public class MainActivity extends AppCompatActivity {
+OneTimeWorkRequest oneTimeWorkRequest;
+TextView textView;
+    public static final String KEY_COUNT_VALUE="key_count";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Data data=new Data.Builder().putInt(KEY_COUNT_VALUE,1750).build();
+
+        Constraints constraints=new Constraints
+                .Builder()
+                .setRequiredNetworkType(NetworkType.CONNECTED).build();
+
+        oneTimeWorkRequest=new OneTimeWorkRequest.Builder(DemoWorker.class)
+                .setInputData(data)
+                .setConstraints(constraints)
+                .build();
+        textView= (TextView) findViewById(R.id.textView);
+
+
+        WorkManager.getInstance(this).getWorkInfoByIdLiveData(oneTimeWorkRequest.getId())
+                .observe(this, new Observer<WorkInfo>() {
+                    @Override
+                    public void onChanged(WorkInfo workInfo) {
+                        if(workInfo!=null) {
+                            textView.setText(workInfo.getState().name());
+                            if (workInfo.getState().isFinished()) {
+
+                                Data data1 = workInfo.getOutputData();
+                                String message = data1.getString(DemoWorker.KEY_WORKER);
+                                Toast.makeText(getApplicationContext(), message, Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    }
+                });
+    }
+
+    public void onClick(View view) {
+        WorkManager.getInstance(this).enqueue(oneTimeWorkRequest);
+    }
+}
